d1e5689c754b19f34946891a9131972d54a6d879
DanyZurita <dzurita@cifpfbmoll.eu>
2020-06-07 22:31:24 +0200

    feat(ServeiAlumnat): Añadir tag autowired del repository y lógica de matricula() para caso test

--- semana1/src/main/java/org/formacio/component/ServeiAlumnat.java
+++ semana1/src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,11 +1,13 @@
 package org.formacio.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServeiAlumnat {
 
-	
+	@Autowired
+	RepositoriAlumnesMemoria repo;
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
 	 * el corresponent codi.
@@ -13,7 +15,13 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
-		return true;
+		if (alumne != null) {
+			repo.altaAlumne(id, alumne);
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 }
