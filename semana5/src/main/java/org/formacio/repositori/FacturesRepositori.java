916cb8dafd66c3f73c7229c73ea9c97df5b32ea0
DanyZurita <dzurita@cifpfbmoll.eu>
2020-06-09 15:21:59 +0200

    feat(FacturaRepository): Añadir extension de CrudRepository, Query de totalClient() y findByClientName()

--- semana5/src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ semana5/src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,8 +1,16 @@
 package org.formacio.repositori;
 
+import java.util.List;
 
-public interface FacturesRepositori  {
+import org.formacio.domain.Factura;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
+public interface FacturesRepositori extends CrudRepository<Factura, Long> {
+	
+	@Query("select sum(linia.total) from Factura factura join factura.linies linia where factura.client.nom = ?1")
 	public Number totalClient(String client);
+	
+	public List<Factura> findByClientNom(String nom);
 	
 }
